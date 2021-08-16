5d27983d51d983e2bc19ee3828b3a974e05a73ee
Dmitrii_Pikhurov <Dmitrii_Pikhurov@epam.com>
2021-08-16 21:26:32 +0300

    Add correct constants names.

--- src/test/java/com/epam/tc/hw2/TestControlElementsOfElementsPage.java
+++ src/test/java/com/epam/tc/hw2/TestControlElementsOfElementsPage.java
@@ -12,19 +12,19 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 
-public class TestControlElementsOfElementsPage extends AbstractTestPage{
+public class TestControlElementsOfElementsPage extends AbstractTestPage {
 
 
-    private final String expectedPageTitle = "Home Page";
-    private final String expectedUserName = "ROMAN IOVLEV";
-    private final Map<String, String> expectedLogNameStatus = Map.of(
+    private static final  String EXPECTED_PAGE_TITLE = "Home Page";
+    private static final String EXPECTED_USER_NAME = "ROMAN IOVLEV";
+    private static final Map<String, String> EXPECTED_LOG_NAME_STATUS = Map.of(
         "Water", "true",
         "Wind", "true",
         "metal", "Selen",
         "Colors", "Yellow");
-    private final String expectedSelectColor = "Yellow";
-    private final int orderOfNameWordInLog = 1;
-    private final int orderOfStatusWordInLog = 5;
+    private static final String EXPECTED_SELECT_COLOR = "Yellow";
+    private static final int ORDER_OF_NAME_WORD_IN_LOG = 1;
+    private static final int ORDER_OF_STATUS_WORD_IN_LOG = 5;
 
 
     @BeforeClass
@@ -38,7 +38,7 @@ public class TestControlElementsOfElementsPage extends AbstractTestPage{
         // 1. Open test site by URL
         webDriver.navigate().to("https://jdi-testing.github.io/jdi-light/index.html");
         // 2. Assert Browser title
-        Assertions.assertThat(webDriver.getTitle()).isEqualTo(expectedPageTitle);
+        Assertions.assertThat(webDriver.getTitle()).isEqualTo(EXPECTED_PAGE_TITLE);
         // 3. Perform login
         webDriver.findElement(By.id("user-icon")).click();
         webDriver.findElement(By.id("name")).sendKeys("Roman");
@@ -47,7 +47,7 @@ public class TestControlElementsOfElementsPage extends AbstractTestPage{
         // 4. Assert User name in the left-top side of screen that user is loggined
         WebElement username = webDriver.findElement(By.id("user-name"));
         Assertions.assertThat(username.isDisplayed()).isTrue();
-        Assertions.assertThat(username.getText()).isEqualTo(expectedUserName);
+        Assertions.assertThat(username.getText()).isEqualTo(EXPECTED_USER_NAME);
         // 5. Open through the header menu Service -> Different Elements Page
         webDriver.findElement(
             By.cssSelector("ul[class=\"uui-navigation nav navbar-nav m-l8\"]>li>a[class=\"dropdown-toggle\"]")).click();
@@ -64,17 +64,17 @@ public class TestControlElementsOfElementsPage extends AbstractTestPage{
         // 8. Select in dropdown
         WebElement selectColor = webDriver.findElement(By.cssSelector("select[class=\"uui-form-element\"]"));
         Select selectColorDropdown = new Select(selectColor);
-        selectColorDropdown.selectByVisibleText(expectedSelectColor);
+        selectColorDropdown.selectByVisibleText(EXPECTED_SELECT_COLOR);
         // 9. Assert Log rows are correct
         List<WebElement> panelListLogs =
             webDriver.findElements(By.cssSelector("ul[class=\"panel-body-list logs\"]>li"));
         Map<String, String> logNameStatus = new HashMap<>();
         for (WebElement panelListLog : panelListLogs) {
-            String itemName = panelListLog.getText().split("\\p{Punct}? +")[orderOfNameWordInLog];
-            String itemStatus = panelListLog.getText().split("\\p{Punct}? +")[orderOfStatusWordInLog];
+            String itemName = panelListLog.getText().split("\\p{Punct}? +")[ORDER_OF_NAME_WORD_IN_LOG];
+            String itemStatus = panelListLog.getText().split("\\p{Punct}? +")[ORDER_OF_STATUS_WORD_IN_LOG];
             logNameStatus.put(itemName, itemStatus);
         }
-        Assertions.assertThat(logNameStatus).isEqualTo(expectedLogNameStatus);
+        Assertions.assertThat(logNameStatus).isEqualTo(EXPECTED_LOG_NAME_STATUS);
         // 10. Close Browser
         webDriver.quit();
     }
--- src/test/java/com/epam/tc/hw2/TestControlElementsOfHomePage.java
+++ src/test/java/com/epam/tc/hw2/TestControlElementsOfHomePage.java
@@ -6,7 +6,6 @@ import java.util.stream.Collectors;
 import org.assertj.core.api.SoftAssertions;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -15,20 +14,20 @@ public class TestControlElementsOfHomePage extends AbstractTestPage {
 
 
     private SoftAssertions softAssertions;
-    private final List<String> expectedNavigatorBarItems = new ArrayList<>(
+    private static final List<String> EXPECTED_NAVIGATOR_BAR_ITEMS = new ArrayList<>(
         List.of("HOME", "CONTACT FORM", "SERVICE", "METALS & COLORS"));
-    private final List<String> expectedBenefitTexts = new ArrayList<>(List.of("To include good practices\n"
+    private static final List<String> EXPECTED_BENEFIT_TEXTS = new ArrayList<>(List.of("To include good practices\n"
         + "and ideas from successful\n"
         + "EPAM project", "To be flexible and\n"
         + "customizable", "To be multiplatform", "Already have good base\n"
         + "(about 20 internal and\n"
         + "some external projects),\n"
         + "wish to get more…"));
-    private final List<String> expectedSidebarElements = new ArrayList<>(
+    private static final List<String> EXPECTED_SIDEBAR_ELEMENTS = new ArrayList<>(
         List.of("Home", "Contact form", "Service", "Metals & Colors", "Elements packs"));
-    private final int expectedIconsNumber = 4;
-    private final String expectedUserName = "ROMAN IOVLEV";
-    private final String expectedPageTitle = "Home Page";
+    private static final int EXPECTED_ICONS_NUMBER = 4;
+    private static final String EXPECTED_USER_NAME = "ROMAN IOVLEV";
+    private static final String EXPECTED_PAGE_TITLE = "Home Page";
 
     @BeforeMethod
     void createSoftAssertion() {
@@ -40,7 +39,7 @@ public class TestControlElementsOfHomePage extends AbstractTestPage {
         // 1. Open test site by URL
         webDriver.navigate().to("https://jdi-testing.github.io/jdi-light/index.html");
         // 2. Assert Browser title
-        softAssertions.assertThat(webDriver.getTitle()).isEqualTo(expectedPageTitle);
+        softAssertions.assertThat(webDriver.getTitle()).isEqualTo(EXPECTED_PAGE_TITLE);
         // 3. Perform login
         webDriver.findElement(By.id("user-icon")).click();
         webDriver.findElement(By.id("name")).sendKeys("Roman");
@@ -49,18 +48,18 @@ public class TestControlElementsOfHomePage extends AbstractTestPage {
         // 4. Assert Username is loggined
         WebElement username = webDriver.findElement(By.id("user-name"));
         softAssertions.assertThat(username.isDisplayed()).isTrue();
-        softAssertions.assertThat(username.getText()).isEqualTo(expectedUserName);
+        softAssertions.assertThat(username.getText()).isEqualTo(EXPECTED_USER_NAME);
         // 5. Assert that there are 4 items on the header section are displayed and they have proper texts
         List<WebElement> navigationBarItems = webDriver.findElements(
             By.cssSelector("ul[class=\"uui-navigation nav navbar-nav m-l8\"]>li"));
-        softAssertions.assertThat(navigationBarItems.size()).isEqualTo(expectedNavigatorBarItems.size());
-        assertThatAllElementsExpectedlyDisplayed(navigationBarItems, expectedNavigatorBarItems);
+        softAssertions.assertThat(navigationBarItems.size()).isEqualTo(EXPECTED_NAVIGATOR_BAR_ITEMS.size());
+        assertThatAllElementsExpectedlyDisplayed(navigationBarItems, EXPECTED_NAVIGATOR_BAR_ITEMS);
         // 6. Assert that there are 4 images on the Index Page and they are displayed
         List<WebElement> benefitIcons = webDriver.findElements(By.cssSelector("span[class^=\"icons-benefit\"]"));
-        softAssertions.assertThat(benefitIcons.size()).isEqualTo(expectedIconsNumber);
+        softAssertions.assertThat(benefitIcons.size()).isEqualTo(EXPECTED_ICONS_NUMBER);
         // 7. Assert that there are 4 texts on the Index Page under icons and they have proper text
         List<WebElement> benefitTexts = webDriver.findElements(By.cssSelector("span[class=\"benefit-txt\"]"));
-        assertThatAllElementsExpectedlyDisplayed(benefitTexts, expectedBenefitTexts);
+        assertThatAllElementsExpectedlyDisplayed(benefitTexts, EXPECTED_BENEFIT_TEXTS);
         // 8. Assert that there is the iframe with “Frame Button” exist
         WebElement buttonFrame = webDriver.findElement(By.id("frame"));
         softAssertions.assertThat(buttonFrame.isDisplayed()).isTrue();
@@ -72,8 +71,8 @@ public class TestControlElementsOfHomePage extends AbstractTestPage {
         webDriver.switchTo().parentFrame();
         // 11. Assert that there are 5 items in the Left Section are displayed and they have proper text
         List<WebElement> sidebarElements = webDriver.findElements(By.cssSelector("ul[class=\"sidebar-menu left\"]>li"));
-        softAssertions.assertThat(sidebarElements.size()).isEqualTo(expectedSidebarElements.size());
-        assertThatAllElementsExpectedlyDisplayed(sidebarElements, expectedSidebarElements);
+        softAssertions.assertThat(sidebarElements.size()).isEqualTo(EXPECTED_SIDEBAR_ELEMENTS.size());
+        assertThatAllElementsExpectedlyDisplayed(sidebarElements, EXPECTED_SIDEBAR_ELEMENTS);
         softAssertions.assertAll();
         // 12. Close Browser
         webDriver.quit();
