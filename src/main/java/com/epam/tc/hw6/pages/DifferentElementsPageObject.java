afed60cca59deb252f556424993139ae21e0485d
Dmitrii_Pikhurov <Dmitrii_Pikhurov@epam.com>
2021-08-28 18:08:58 +0300

    Refactoring for remote selenium node execution under chrome and firefox browsers.

---
+++ src/main/java/com/epam/tc/hw6/pages/DifferentElementsPageObject.java
@@ -0,0 +1,53 @@
+package com.epam.tc.hw6.pages;
+
+import com.epam.tc.hw6.pages.components.elements.ElementsPageCheckboxRow;
+import com.epam.tc.hw6.pages.components.elements.ElementsPageColorSelector;
+import com.epam.tc.hw6.pages.components.elements.ElementsPageLogPanel;
+import com.epam.tc.hw6.pages.components.elements.ElementsPageRadioRaw;
+import com.epam.tc.hw6.pages.components.homepage.HomePageLoginMenu;
+import java.util.Map;
+import org.openqa.selenium.WebDriver;
+
+public class DifferentElementsPageObject extends AbstractPageObject {
+
+    private HomePageLoginMenu loginMenu;
+    private ElementsPageCheckboxRow checkboxRow;
+    private ElementsPageRadioRaw radioRaw;
+    private ElementsPageColorSelector colorSelector;
+    private ElementsPageLogPanel logPanel;
+
+    public DifferentElementsPageObject(WebDriver driver) {
+        super(driver);
+        checkboxRow = new ElementsPageCheckboxRow(driver);
+        radioRaw = new ElementsPageRadioRaw(driver);
+        colorSelector = new ElementsPageColorSelector(driver);
+        logPanel = new ElementsPageLogPanel(driver);
+        loginMenu = new HomePageLoginMenu(driver);
+    }
+
+    @Override
+    public void logIn(String loginData, String passwordData) {
+        loginMenu.logIn(loginData, passwordData);
+    }
+
+    
+    public void clickElementsCheckBoxWithText(String text) {
+        checkboxRow.clickCheckboxWithText(text);
+    }
+
+    
+    public void clickElementsRadioWithText(String text) {
+        radioRaw.clickElementsRadioWithText(text);
+    }
+
+    
+    public void chooseColorDropdownMenu(String color) {
+        colorSelector.chooseColor(color);
+    }
+
+    
+    public Map<String, String> getMapOfLogsFromLogPanel() {
+        return logPanel.getMapOfLogs();
+    }
+
+}
---
+++ src/main/java/com/epam/tc/hw6/pages/components/elements/ElementsPageCheckboxRow.java
@@ -0,0 +1,27 @@
+package com.epam.tc.hw6.pages.components.elements;
+
+import com.epam.tc.hw6.pages.components.AbstractComponent;
+import java.util.List;
+import java.util.function.Predicate;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class ElementsPageCheckboxRow extends AbstractComponent {
+
+    @FindBy(className = "label-checkbox")
+    List<WebElement> elementsCheckboxes;
+
+    public ElementsPageCheckboxRow(WebDriver driver) {
+        super(driver);
+    }
+
+    public void clickCheckboxWithText(String text) {
+        Predicate<String> predicate = element -> element.equals(text);
+        elementsCheckboxes.stream()
+                      .filter(webElement -> predicate.test(webElement.getText()))
+                      .findFirst()
+                      .orElseThrow()
+                      .click();
+    }
+}
