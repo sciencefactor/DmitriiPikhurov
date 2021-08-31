554494007d907369cf6ec3990e3129f7eee6e343
Dmitrii Pikhurov <dmitrii_pikhurov@epam.com>
2021-08-31 13:39:00 +0300

    Move Given to additional class.

--- src/main/java/com/epam/tc/hw5/pages/DifferentElementsPageObject.java
+++ src/main/java/com/epam/tc/hw5/pages/DifferentElementsPageObject.java
@@ -38,7 +38,4 @@ public class DifferentElementsPageObject extends AbstractPageObject {
         colorSelector.chooseColor(color);
     }
 
-    
-
-
 }
--- src/test/java/com/epam/tc/hw5/tests/cucumber/steps/ActionStep.java
+++ src/test/java/com/epam/tc/hw5/tests/cucumber/steps/ActionStep.java
@@ -8,37 +8,6 @@ import io.qameta.allure.Step;
 
 public class ActionStep extends AbstractStep {
 
-    @Given("I open JDI GitHub site")
-    @Step("Open Home Page site")
-    public void openHomePage() {
-        homePage.openSite();
-    }
-
-    @Given("I login as user \"Roman Iovlev\"")
-    @Step("Login")
-    public void login() {
-        homePage.logIn(TestUsersProviderUtil.getUsernameData(), TestUsersProviderUtil.getPasswordData());
-    }
-
-    @Given("I navigate to Different Elements Page")
-    @Step("Go to Elements Page via Service header element")
-    public void navigateToElementsPage() {
-        homePage.clickNavigationItemService();
-        homePage.clickRefDifferentElements();
-    }
-
-    @Given("I click on \"Service\" button in Header")
-    @Step("Click on \"Service\" button on Home page")
-    public void clickServiceButton() {
-        homePage.clickNavigationItemService();
-    }
-
-    @Given("I click on {string} button in Service dropdown")
-    @Step("Click on button in Service dropdown on Home Page")
-    public void clickHeaderButton(String pageName) {
-        homePage.clickHeaderUserTableButton(pageName);
-    }
-
     @When("I click on {string} checkbox")
     @Step("Click Checkbox with name {text} on Elements page")
     public void clickElementCheckBoxWithText(String text) {
